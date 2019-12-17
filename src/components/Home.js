6ffd92e7fd7ec36b2818ef0bf403662aa9a73a4b
Thomas <tholmes59@gmail.com>
2019-12-17 11:21:49 -0800

    continue to build home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Tom Holmes!</h1>
+        <div className="home-container">
+            <div className="home">
+                <h1>Hello! My name is Tom Holmes and I'm a Developer!</h1> 
+                <h1>Welcome to my Personal Site!</h1>
+            </div>
         </div>
     )
 }
