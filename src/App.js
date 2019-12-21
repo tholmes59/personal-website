c069ef73ad774835842ff993f22142898d58297f
Thomas <tholmes59@gmail.com>
2019-12-21 11:15:16 -0800

    add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import About from "./components/About"
+import Experience from "./components/Experience"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import NavBar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Route>
+          <NavBar />
+        </Route>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/experience" component={Experience}/>
+        <Route exact path="/projects" component={Projects}/>
+        <Route exact path="/contact" component={Contact}/>
+      </Router>
     </div>
   );
 }
