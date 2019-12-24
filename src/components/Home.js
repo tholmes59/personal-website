e30d4d9eb591d5fff00fa13c3ead2db833ec4e61
Thomas <tholmes59@gmail.com>
2019-12-24 12:56:38 -0800

    add button to home component to take to about component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div className="home-container">
             <div className="home">
-                <h1>Hello! My name is Tom Holmes and I'm a Developer!</h1> 
+                <h1>Hello! My name is Tom Holmes</h1>
+                <h1>and I'm a Developer!</h1>
                 <h1>Welcome to my Personal Site!</h1>
+                <button type="button" to="/about" className="home-about-button"><Link className="aboutbutton" to={`/about`}>Learn More About Me</Link></button>
             </div>
         </div>
     )
