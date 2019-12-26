ec22623eada9c76fcb043c91be2ced460f6b0c51
Thomas <tholmes59@gmail.com>
2019-12-25 17:18:07 -0800

    add content to about component

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About!</h1>
+        <div className="about-container">
+            <div className="about">
+                <h1>A little about me!</h1><br/>
+                <p>
+                    Thank you for checking out my site Dear Reader! 
+                </p><br/>
+                <p>
+                My name is Tom Holmes amd I'm a San Francisco based software engineer with passion for the frontend development and a love of all things html, 
+                    css and javascript! I am an experienced professional with a diverse background in both the technology and financial services industries.
+                    I started by journey over a year ago teaching myself the basics of programming 
+                    and recently completed the full stack software engineering program with the Flatiron School. 
+                    Through the Flatiron School’s online software engineering program I gained a deep and diverse skill set in coding languages and frameworks. 
+                    Over the course of the program I learned how to build appliction with HTML, CSS, JavaScript and React/Redux on the frontend and Ruby, Sinatra, and Rails on the backend. 
+                </p><br/>
+                <p>
+                    Along with that skill set, I bring professional experience from my prior career. I previously worked for BlackRock, the world’s largest investment company, 
+                    as a relationship manager partnering with some of the world’s most sophisticated investors to craft solutions for their investment needs. 
+                    I held a similar role at Fisher Investments, working with individual clients to help them reach their investment goals and, prior to that, 
+                    in a research role using data analysis to understand global markets and economics, and help portfolio managers make investment decisions. 
+                    I began my career in the technology industry working for EMC Corporation (now part of Dell Technologies), the world’s largest data storage provider. 
+                    I worked in both finance and product management positions that gave me experience at a global technology company. 
+                    Together, these roles developed my ability to collaborate across teams to achieve results, create solutions based on data, understand what customers need, and most importantly, be a good teammate.
+                </p>
+            </div>
         </div>
     );
 }
