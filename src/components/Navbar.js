50bca61f956d36e5ae80895799e994935674ad9d
Thomas <tholmes59@gmail.com>
2019-12-26 09:09:37 -0800

    create navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,19 @@ const NavBar = () => {
 
     return (
 
+        // <header class="header">
+        //     {/* <a href="" class="logo">CSS Nav</a> */}
+        //     <input class="menu-btn" type="checkbox" id="menu-btn" />
+        //     <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        //     <ul class="menu">
+        //         <li className="nav-item"><NavLink to="/">Home</NavLink></li>
+        //         <li className="nav-item"><NavLink to="/about">About</NavLink></li>
+        //         <li className="nav-item"><NavLink to="/experience">Experience</NavLink></li>
+        //         <li className="nav-item"><NavLink to="/projects">Projects</NavLink></li>
+        //         <li className="nav-item"><NavLink to="/contact">Contact</NavLink></li>
+        //     </ul>
+        // </header>
+
         <nav className="navbar">
             <ul className="nav-links">
                 <li className="nav-item"><NavLink to="/">Home</NavLink></li>
