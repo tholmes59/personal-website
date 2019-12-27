5c0ba35497672a814f201feb578ead7ea6302ae4
Thomas <tholmes59@gmail.com>
2019-12-27 10:52:59 -0800

    build out experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,8 +2,122 @@ import React from 'react';
 
 const Experience = () => {
     return (
-        <div>
-            <h1>Experience!</h1>
+        <div className="experience-container">
+            <div className="experience">
+                <div className="resume-pdf-container">
+                    <a href="https://drive.google.com/open?id=1DztcGQpl2u-AD7c4GoB_2_c8XRqTrEI3" target="_blank" rel="noopener noreferrer" className="resume-pdf"><span>&#10515;</span> Download PDF</a>
+                </div>
+                <h1>Experience</h1><br/>
+                <h3>SUMMARY</h3>
+                <p>Accomplished and hardworking professional looking to transfer analytical and interpersonal skills 
+                    acquired from experience in the technology and financial services industries into a junior software engineering role or apprenticeship opportunity
+                </p><br/>
+                    <h3>KEY SKILLS</h3>
+                    <div className="key-skills">
+                        <div className="key-skills-column">
+                            <ul> 
+                                <li>Web Development</li>
+                                <li>Object-Oriented Programming</li>
+                                <li>Project Managment</li>
+                            </ul>
+                        </div>
+                        <div className="key-skills-column">
+                            <ul>
+                                <li>Strong Collaborative Skills</li>
+                                <li>Excellent Interpersonal Skills</li>
+                                <li>Strong Problem Solving Ability</li>
+                            </ul>
+                        </div>
+                    </div><br/>
+                <h3>TECHNICAL SKILLS</h3>
+                    <div className="technical-skills">
+                        <div className="technical-skills-column">
+                            <ul> 
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                            </ul>
+                        </div>
+                        <div className="technical-skills-column">
+                            <ul> 
+                                <li>React.js</li>
+                                <li>Redux</li>
+                                <li>AJAX</li>
+                            </ul>
+                        </div>
+                        <div className="technical-skills-column">
+                            <ul> 
+                                <li>JSON</li>
+                                <li>Ruby</li>
+                                <li>Ruby on Rails</li>
+                            </ul>
+                        </div>
+                        <div className="technical-skills-column">
+                            <ul> 
+                                <li>Sinatra</li>
+                                <li>SQL</li>
+                                <li>Git</li>
+                            </ul>
+                        </div>
+                    </div><br/>
+                    <h3>EDUCATION</h3>
+                    <p><strong>Flatiron School</strong>, Online Remote</p>
+                    <p>Immersive Full Stack Software Engineering Program, 2019</p>
+                    <ul>
+                        <li>Completed portfolio projects and assessments in relevant industry languages and frameworks</li>
+                        <li>Performed programming challenges and labs to demonstrate language proficiency</li>
+                    </ul><br/>
+                    <p><strong>University of Massachusetts</strong>, Amherst, MA</p>
+                    <p>Bachelor of Science, 1999</p><br/>
+                    <h3>PROFESSIONAL EXPERIENCE</h3>
+                    <p><strong>BlackRock, Inc.</strong>, San Francisco, CA 2014 - 2019</p>
+                    <hr></hr>
+                    <p><i>Relationship Manager – Institutional Client Business</i></p>
+                    <ul>
+                        <li>Built, managed and supported strategic relationships with public and corporate pension funds, foundations and endowments averaging $800 million in assets managed by BlackRock</li>
+                        <li>Delivered strategic solutions to address complex client objectives by collaborating with internal and external business partners</li>
+                        <li>Developed and maintained a strong understanding of BlackRock’s product suite, competitor offerings, market views and client objectives to position and deliver solutions using a consultative approach</li>
+                        <li>Led and coordinated the request for proposal (RFP) process across multiple teams for new client opportunities</li>
+                        <li>Collaborated with investment, operational, legal and regulatory teams across the organization to meet client needs</li>
+                        <li>Led client meetings and presentations</li>
+                    </ul><br/>
+                    <p><strong>Fisher Investments</strong>, Woodside, CA 2006 - 2014</p>
+                    <hr></hr>
+                    <p><i>Relationship Manager – Private Client Group</i> 2011-2014</p>
+                    <ul>
+                        <li>Served as direct liaison with high net worth clients, managing 175 client relationships with assets of $200 million and a 93% retention rate</li>
+                        <li>Maintained ongoing communication with clients, provided current market outlook and educated clients on Fisher Investments’ portfolio strategy</li>
+                        <li>Assessed clients’ financial objectives and suitability to determine optimal product allocation</li>
+                        <li>Served as an integral part of sales strategy that secured over $50 million of new assets under management</li>
+                    </ul><br/>
+                    <p><i>Research Associate – Portfolio Management Group</i> 2006-2011</p>
+                    <ul>
+                        <li>Analyzed the energy, health care and industrials sectors for actively managed portfolios with a total of $40 billion in assets under management</li>
+                        <li>Generated portfolio analytics and performance calculations in the RFP process for prospective clients</li>
+                        <li>Created custom portfolios totaling $100 million for clients for which traditional portfolios were unsuitable due to client restrictions or capital gains impact</li>
+                    </ul><br/>
+                    <p><strong>EMC Corporation</strong>, Hopkinton, MA 2000 - 2005</p>
+                    <hr></hr>
+                    <p><i>Financial Analyst – Storage Platform Operations</i> 2003-2005</p>
+                    <ul>
+                        <li>Prepared budgets and forecasts for engineering group with $108 million of overhead, $27 million of capital equipment and headcount of 650</li>
+                        <li>Selected for business development team to create financial model and business plan for EMC’s $100 million offshore investment</li>
+                        <li>Designed inventory reduction strategies with business partners to reduce inventory balance sheet exposure and increase corporate inventory turnover to 7.2</li>
+                    </ul><br/>
+                    <p><i>Advanced Project Coordinator – Advanced Manufacturing Development</i> 2000-2003</p>
+                    <ul>
+                        <li>Coordinated projects in a simulated manufacturing environment for the development and qualification of EMC’s new storage platform products</li>
+                        <li>Managed the build, test, and delivery of 425 prototype storage systems to transition the latest release of the trademark Symmetrix Data Storage System from design engineering to manufacturing, meeting all corporate deadlines</li>
+                        <li>Reduced average project idle time 60% from 25 days to 10 days in order to improve time-to-market and time-to-profit for new product designs</li>
+                    </ul><br/>
+                    
+                </div>
+                <div className="expereince-fa">
+                    <a href="https://www.linkedin.com/in/thomas-holmes-64426529/" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i></a>
+                    <a href="https://github.com/tholmes59" target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i></a>
+                    <a href="https://twitter.com/THolmsie" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i></a>
+                    <a href="https://www.instagram.com/tom_holmes59/" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram"></i></a>
+                </div>
         </div>
     );
 };
