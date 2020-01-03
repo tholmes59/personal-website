9df8a951fd17f9052f5d406af97259ffae59eafd
Thomas <tholmes59@gmail.com>
2020-01-03 09:01:50 -0800

    edit div classnames

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
                 <img src={recipe} alt="recipe finder" className="project-item-img"/>
                 <p className="project-title">Recipe Finder</p>
                 <div className="project-overlay"></div>
-                <div class="project-button">
+                <div className="project-button">
                     <a href="https://github.com/tholmes59/recipe-finder-frontend.git" target="_blank" rel="noopener noreferrer">Github</a>
                     <a href="https://drive.google.com/open?id=1YC61DAm99lLXjq7kaaSueg-r3S1UQ2nv" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
@@ -23,7 +23,7 @@ const Projects = () => {
                 <img src={fitness} alt="fitness app" className="project-item-img"/>
                 <p className="project-title">Fitness App</p>
                 <div className="project-overlay"></div>
-                <div class="project-button">
+                <div className="project-button">
                     <a href="https://github.com/tholmes59/fitness-app" target="_blank" rel="noopener noreferrer">Github</a>
                     <a href="https://drive.google.com/open?id=1A_5WAZq63dudWBj0aCqbDfXDNfEjcH47" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
@@ -32,7 +32,7 @@ const Projects = () => {
                 <img src={fitnessjs} alt="fitness app with js" className="project-item-img"/>
                 <p className="project-title">Fitness App w/ js</p>
                 <div className="project-overlay"></div>
-                <div class="project-button">
+                <div className="project-button">
                     <a href="https://github.com/tholmes59/fitness-app-js" target="_blank" rel="noopener noreferrer">Github</a>
                     <a href="https://drive.google.com/open?id=1KItdynB9GHr9sAYiTKNlKdSfv4m7uz60" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
@@ -41,7 +41,7 @@ const Projects = () => {
                 <img src={sinatra} alt="sinatra wine app" className="project-item-img"/>
                 <p className="project-title">Sinatra Wine App</p>
                 <div className="project-overlay"></div>
-                <div class="project-button">
+                <div className="project-button">
                     <a href="https://github.com/tholmes59/sinatra-wine-app" target="_blank" rel="noopener noreferrer">Github</a>
                     <a href="https://drive.google.com/open?id=1g0Th6Q-3_Buvb0QD7Z-mNrMkTX-YgfAD" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
@@ -50,7 +50,7 @@ const Projects = () => {
                 <img src={celtics} alt="celtics roster" className="project-item-img"/>
                 <p className="project-title">Celtics Roster</p>
                 <div className="project-overlay"></div>
-                <div class="project-button">
+                <div className="project-button">
                     <a href="https://github.com/tholmes59/celtics-roster-cli-project" target="_blank" rel="noopener noreferrer">Github</a>
                     <a href="https://drive.google.com/open?id=1Ip4hY0_xTlX2SFCeSje85CoAZnsa1eOS" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
