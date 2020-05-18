b9bbde5c7d78151e36459271707ef5c58fdaf7a3
Thomas <tholmes59@gmail.com>
2020-05-18 11:24:29 -0700

    add scroll to top to app.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Contact from "./components/Contact"
 import NavBar from './components/Navbar';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ScrollUpButton from "react-scroll-up-button";
+import ScrollToTop from './components/ScrollToTop.js';
 
 function App() {
   return (
     <div className="App">
       <ScrollUpButton />
       <Router>
+      <ScrollToTop>
         <Route>
           <NavBar />
         </Route>
@@ -22,6 +24,7 @@ function App() {
         <Route exact path="/experience" component={Experience}/>
         <Route exact path="/projects" component={Projects}/>
         <Route exact path="/contact" component={Contact}/>
+        </ScrollToTop>
       </Router>
     </div>
   );
